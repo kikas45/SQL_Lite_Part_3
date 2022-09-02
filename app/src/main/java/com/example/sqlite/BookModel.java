29aa6bb318948e98960079bcfb27c08acf7f6dac
kikas45 <tigera1759@gmail.com>
2022-09-02 16:05:39 +0100

    SQL_Lite_Part 3

---
+++ app/src/main/java/com/example/sqlite/BookModel.java
@@ -0,0 +1,59 @@
+package com.example.sqlite;
+
+public class BookModel {
+    private int id;
+    private String image;
+    private String title;
+    private String desc;
+    private String icon;
+    private String name;
+
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
